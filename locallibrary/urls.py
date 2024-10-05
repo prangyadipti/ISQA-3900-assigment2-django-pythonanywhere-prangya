4ef704d9d021a877265e43986c0bcfddff85316c
prangyadipti <pchoudhury@unomaha.edu>
2024-10-05 17:08:12 -0500

    setting.py update
    setting.py update for pythonanywhere
    debug=false

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
@@ -37,6 +38,10 @@ urlpatterns = [
 
     # Authentication URLs (login, logout, etc.)
     path('accounts/', include('django.contrib.auth.urls')),   # Only need this once
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),  # serve media files when deployed
+
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    # serve static files when deployed
 ]
 
 # Serve media files during development
