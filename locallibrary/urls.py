2781873ac1d89b2a7e41dbbc57fa4283e7b61412
prangyadipti <pchoudhury@unomaha.edu>
2024-10-05 16:54:33 -0500

    Assignment 2 pythonanywhere
    Assignment 2 Pythonanywhere deployment

---
+++ locallibrary/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for locallibrary project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    # Forgot password - reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    # Admin URL
+    path('admin/', admin.site.urls),
+
+    # Include app-specific URLs
+    path('', include('catalog.urls')),      # Catalog URLs for the main app
+    path('register/', include('register.urls')),  # Register app
+
+    # Authentication URLs (login, logout, etc.)
+    path('accounts/', include('django.contrib.auth.urls')),   # Only need this once
+]
+
+# Serve media files during development
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
